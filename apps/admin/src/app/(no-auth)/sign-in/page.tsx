c3b84812e4189f2a213f89178cb4105cae9b2c3b
AshwithJoylan <45203278+AshwithJoylan@users.noreply.github.com>
2025-10-12 12:30:58 +0530

    changes

--- apps/admin/src/app/(no-auth)/sign-in/page.tsx
+++ apps/admin/src/app/(no-auth)/sign-in/page.tsx
@@ -13,7 +13,7 @@ export default function LoginPage() {
             <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
               <GalleryVerticalEnd className="size-4" />
             </div>
-            eTroupers
+            Junglexp
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
